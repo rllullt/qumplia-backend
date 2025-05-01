4230ef48e4c28dd05abf90fc2060f0f43f4ab4f0
Rodrigo Llull Torres <rllullt@gmail.com>
2025-05-01 14:39:40 -0400

    CORS Support for Django REST

--- compliance/settings.py
+++ compliance/settings.py
@@ -44,12 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apps.campaigns',
     'apps.users',
     'apps.compliance_core',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -92,10 +94,10 @@ DATABASES = {
              env('DB_ENGINE', default='sqlite3')
          ),
         'NAME': str(env('DB_NAME', default=BASE_DIR / 'db.sqlite3')),
-        'USER': env('DB_USER', default='compliance_user'),
+        'USER': env('DB_USER', default=''),
         'PASSWORD': env('DB_PASSWORD', default=''),
-        'HOST': env('DB_HOST', default='127.0.0.1'),
-        'PORT': env('DB_PORT', default=5432),
+        'HOST': env('DB_HOST', default=''),
+        'PORT': env('DB_PORT', default=''),
     }
 }
 
@@ -140,3 +142,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # Adjustable to your needs
+        # 'rest_framework.permissions.IsAuthenticated',  # Uncomment to require authentication
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.BrowsableAPIRenderer',  # Interactive API browser
+        'rest_framework.renderers.JSONRenderer',  # Allows JSON output
+    ),
+}
+
+# CORS settings (fixed for accepting requests from frontend)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://qumplia-front:80",
+]
