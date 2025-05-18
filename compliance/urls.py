6a8c72deb1b323ebce71d872e3a4841bebb78113
Rodrigo Llull Torres <rllullt@gmail.com>
2025-05-17 23:32:02 -0400

    Fix url for 'api/' inlcudes in project level urls.py

--- compliance/urls.py
+++ compliance/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api', include(router.urls)),
+    path('api/', include(router.urls)),
     # Browsable API, REST’s login and logout views
     path('api-auth/', include('rest_framework.urls')),
     path('api/campaigns/', include('apps.campaigns.urls')),
